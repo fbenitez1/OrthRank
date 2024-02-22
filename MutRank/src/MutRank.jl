3b3ff001306eb56624aac0e1d770050dff7f5bac
Felipe Benitez <fbenitez1@gsu.edu>
2024-02-21 21:43:21 -0500

    Fixed bug with block of size of the matrix

--- MutRank/src/MutRank.jl
+++ MutRank/src/MutRank.jl
@@ -21,14 +21,6 @@ macro swap(x,y)
      end
 end
 
-function summax(x,y,bound)
-    if x+y <= bound
-        return x+y
-    else 
-        return bound
-    end
-end
-
  function random_blocks(
     n::Int64, #size of matrix
     nb::Int64, #Number of blocks
@@ -81,6 +73,9 @@ function preparative_phase!(
     )
     m, n = size(gw.b)
     ub_g_ind = length(gw.b.upper_blocks)
+    while gw.b.upper_blocks[ub_g_ind].mb == m
+        ub_g_ind -=1
+    end
     ub_mb = gw.b.upper_blocks[ub_g_ind].mb
     for lb_ind in filter_compressed(Iterators.Reverse(gw.b.lower_blocks))    
         lb_g_ind = gw.b.lower_blocks[lb_ind].givens_index
@@ -98,7 +93,7 @@ function preparative_phase!(
                     apply!(ub_vb,ub_rot)
                 end
                 #Reduce fillin of expansion.
-                for fillin_row in (ub_mb+1):(lb_rot.inds+lb_rank)
+                for fillin_row in ub_mb+1:lb_rot.inds+lb_rank
                     last_column_el = last_inband_index(gw.b,fillin_row,:)
                     while gw.b[fillin_row,last_column_el]==0
                         last_column_el -=1
@@ -292,75 +287,4 @@ function get_QR(
     return Q, gw
 end
 
-# m=10
-# num_blocks=10
-# upper_rank_max=2
-# lower_rank_max=2
-# n = m
-# upper_blocks, lower_blocks = random_blocks(m,num_blocks)
-# upper_ranks = Consts(num_blocks, upper_rank_max)
-# lower_ranks = Consts(num_blocks, lower_rank_max)
-# upper_ranks = constrain_upper_ranks(m, n, blocks = upper_blocks, ranks = upper_ranks)
-# lower_ranks = constrain_lower_ranks(m, n, blocks = lower_blocks, ranks = lower_ranks)
-# max_num_upper_rots = 2*lower_rank_max * (2*(n÷num_blocks - 1) + upper_rank_max ) + 2*(n÷num_blocks - 1) + upper_rank_max
-# max_num_lower_rots = 2*(n÷num_blocks - 1) + lower_rank_max
-# upper_rank_max = 2*(n÷num_blocks - 1) + upper_rank_max
-# lower_rank_max = 2*lower_rank_max
-# max_num_upper_rots = 2*lower_rank_max * (2*(n÷num_blocks - 1) + upper_rank_max ) + 2*(n÷num_blocks - 1) + upper_rank_max
-# max_num_lower_rots = 2*(n÷num_blocks - 1) + lower_rank_max
-# upper_blocks, lower_blocks = random_blocks(n,num_blocks)
-
-
-# m=6
-# n=6
-# lbl = [
-#   2 4 6
-#   2 4 5
-# ]
-# lower_blocks = [BlockSize(lbl[1,j], lbl[2,j]) for j∈1:3]
-
-# ubl = [
-#   0
-#   0
-# ]
-# upper_blocks = [BlockSize(ubl[1,j], ubl[2,j]) for j∈1:1]
-
-# lower_ranks = [2, 2, 0]
-# upper_ranks = [0]
-# upper_rank_max=3
-# lower_rank_max=3
-# num_blocks=3
-# max_num_upper_rots=20
-# max_num_lower_rots=20
-
-# gw1 = GivensWeight(
-#     Float64,
-#     TrailingDecomp(),
-#     MersenneTwister(),
-#     m,
-#     n;
-#     upper_rank_max = upper_rank_max,
-#     lower_rank_max = lower_rank_max,
-#     upper_ranks = upper_ranks,
-#     lower_ranks = lower_ranks, 
-#     upper_blocks = upper_blocks,
-#     max_num_upper_blocks = num_blocks,
-#     lower_blocks = lower_blocks,
-#     max_num_lower_blocks = num_blocks,
-#     max_num_upper_rots = max_num_upper_rots,
-#     max_num_lower_rots = max_num_lower_rots,
-# )
-# println(wilk(gw1.b))
-# rotaciones = copy(gw1.lowerRots)
-# A=Matrix(gw1) 
-# A_or=copy(A)
-# preparative_phase!(gw1)
-#triang_rot = residual_phase!(gw1)
-# QB = Matrix(1.0I,n,n)
-# QT = Matrix(1.0I,n,n)
-# apply_rotations!(triang_rot,QT)
-# apply_rotations_reverse!(gw1.lowerRots[:,1:3],QB)
-# gw1.lowerRots[:,1:3] .= Rot(one(Float64), zero(Float64), 1)
-# er=norm(QB*A - Matrix(gw1), Inf)
-# print("Norm: $er \n")    
 end #module
